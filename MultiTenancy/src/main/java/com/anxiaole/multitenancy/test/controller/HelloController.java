6e40d758da311dd6c8a19b62bca88af794f926c6
LiuXianfa <xianfaliu@newbanker.cn>
2021-04-11 00:32:43 +0800

    MultiTenancy 修改租户数据库配置的接口.

--- MultiTenancy/src/main/java/com/anxiaole/multitenancy/test/controller/HelloController.java
+++ MultiTenancy/src/main/java/com/anxiaole/multitenancy/test/controller/HelloController.java
@@ -1,10 +1,15 @@
 package com.anxiaole.multitenancy.test.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.anxiaole.multitenancy.TenantIdHolder;
 import com.anxiaole.multitenancy.test.dao.UserDao;
 import com.anxiaole.multitenancy.test.mock.MockAddTenant;
+import com.anxiaole.multitenancy.utils.Utils;
 
+import org.I0Itec.zkclient.ZkClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,5 +39,45 @@ public class HelloController {
     public String hello() {
         String name = userDao.findFirstUserName();
         return String.format("(当前租户id:%s)   你好:%s.    ", TenantIdHolder.getTenantId(), name);
+    }
+
+
+    @Autowired
+    ZkClient zkClient;
+
+    /**
+     * <pre>
+     * 修改租户数据源配置.
+     * 可以把数据源配置修改错误,然后在调用上面的接口试一下.
+     * 主要是为了测试 配置修改后,数据源会不会重新初始化.
+     *
+     * 注意:还需要在请求头或者请求参数中传入tenantId
+     * </pre>
+     *
+     * @param username
+     * @param password
+     * @param url
+     * @param driverclassname
+     * @return
+     */
+    @RequestMapping("changeJdbc")
+    public JSONObject changeJdbc(String username, String password, String url, String driverclassname) {
+        String path = Utils.tenantIdToPath(TenantIdHolder.getTenantId());
+        String jdbc = zkClient.readData(path);
+        JSONObject jsonObject = JSON.parseObject(jdbc);
+        if (StringUtils.hasText(username)) {
+            jsonObject.put("username", username);
+        }
+        if (StringUtils.hasText(password)) {
+            jsonObject.put("password", password);
+        }
+        if (StringUtils.hasText(url)) {
+            jsonObject.put("url", url);
+        }
+        if (StringUtils.hasText(driverclassname)) {
+            jsonObject.put("driverclassname", driverclassname);
+        }
+        zkClient.writeData(path, JSON.toJSONString(jsonObject));
+        return JSON.parseObject(zkClient.readData(path));
     }
 }
