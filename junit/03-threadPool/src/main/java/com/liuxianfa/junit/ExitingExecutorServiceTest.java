d7be9464210b32079b58282d587b48a884c840d9
LiuXianfa <745024471@qq.com>
2022-01-29 22:39:13 +0800

    线程池

--- junit/03-threadPool/src/main/java/com/liuxianfa/junit/ExitingExecutorServiceTest.java
+++ junit/03-threadPool/src/main/java/com/liuxianfa/junit/ExitingExecutorServiceTest.java
@@ -2,6 +2,7 @@ package com.liuxianfa.junit;
 
 import com.google.common.util.concurrent.MoreExecutors;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
 
@@ -18,7 +19,8 @@ public class ExitingExecutorServiceTest {
     public static void main(String[] args) {
 //        guava守护线程池();
 
-        使用守护线程工厂();
+        使用CompletableFuture实现();
+//        使用守护线程工厂();
 
 //        默认情况下JDK线程池中的任务都执行完毕之后JVM也不会退出();
 
@@ -31,15 +33,35 @@ public class ExitingExecutorServiceTest {
             sleep(4);
             System.out.println("线程执行完毕." + Thread.currentThread().getName());
         });
-        System.out.println("1");
+        System.out.println("第1个任务提交完毕.");
 
 
         JDK_THREAD_POOL.execute(() -> {
             sleep(4);
             System.out.println("线程执行完毕." + Thread.currentThread().getName());
         });
-        System.out.println("2");
+        System.out.println("第2个任务提交完毕.");
     }
+
+
+    private static  void 使用CompletableFuture实现(){
+        CompletableFuture<Void> future1 = CompletableFuture.runAsync(() -> {
+            sleep(4);
+            System.out.println("线程执行完毕." + Thread.currentThread().getName());
+        });
+        System.out.println("第1个任务提交完毕.");
+
+        CompletableFuture<Void> future2 = CompletableFuture.runAsync(() -> {
+            sleep(4);
+            System.out.println("线程执行完毕." + Thread.currentThread().getName());
+        });
+        System.out.println("第2个任务提交完毕.");
+
+
+        // 等待所有future执行完毕.
+        CompletableFuture.allOf(future1, future2).join();
+    }
+
 
     /**
      * 并不会等待两个线程执行完毕之后,再退出jvm进程.
@@ -51,14 +73,14 @@ public class ExitingExecutorServiceTest {
             sleep(4);
             System.out.println("线程执行完毕." + Thread.currentThread().getName());
         });
-        System.out.println("1");
+        System.out.println("第1个任务提交完毕.");
 
 
         DAEMON_JDK_THREAD_POOL.execute(() -> {
             sleep(4);
             System.out.println("线程执行完毕." + Thread.currentThread().getName());
         });
-        System.out.println("2");
+        System.out.println("第2个任务提交完毕.");
     }
 
     /**
@@ -75,13 +97,13 @@ public class ExitingExecutorServiceTest {
             sleep(4);
             System.out.println("线程执行完毕." + Thread.currentThread().getName());
         });
-        System.out.println("1");
+        System.out.println("第1个任务提交完毕.");
 
         exitingExecutorService.execute(() -> {
             sleep(4);
             System.out.println("线程执行完毕." + Thread.currentThread().getName());
         });
-        System.out.println("2");
+        System.out.println("第2个任务提交完毕.");
     }
 
 
