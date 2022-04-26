ec90e4e94decdc50ec68953b425f26a0fc4f7b13
LiuXianfa <745024471@qq.com>
2022-04-27 00:01:41 +0800

    dfdf

--- junit/09-poi/src/main/java/LatexTest.java
+++ junit/09-poi/src/main/java/LatexTest.java
@@ -8,6 +8,7 @@ import org.ddr.poi.latex.LaTeXRenderPolicy;
 
 import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -26,9 +27,17 @@ public class LatexTest {
     @SneakyThrows
     public static void main(String[] args) {
 
+        System.out.println(getRegex("fad{adb}","(.*?)_\\{(.*?)\\}"));
+        System.out.println(getRegex("asfasf{ad{ddf}dfsdf}","(.*?)_\\{(.*?)\\}"));
+        System.out.println(getRegex("$$ φ_{C_{2}H_{6}·g} $$","(.*?)_\\{(.*?)\\}"));
+        extracted();
+    }
+
+    @SneakyThrows
+    private static void extracted() {
         ArrayList<String> lines = new ArrayList<>();
 //        FileUtil.readLines(LatexTest.class.getResource("latex.txt"), "utf-8", lines);
-        lines.add("ABC_{abc_{123}}");
+        lines.add("φ_{C_{2}H_{6}·g}");
         List<HashMap<Object, Object>> list = lines.stream()
                                                   .map(s -> String.format("$$ %s $$", s))
                                                   .map(latex -> {
@@ -36,9 +45,7 @@ public class LatexTest {
                                                       map.put("row", latex);
                                                       map.put("abc_i", latex);
                                                       map.put("abc", latexTextRm(latex)); // fixme:
-                                                      String htmlFromLatexMath = getHtmlFromLatexMath(latex);
-                                                      System.out.println(htmlFromLatexMath);
-                                                      map.put("abc_html", htmlFromLatexMath);
+                                                      map.put("abc_html", getHtmlFromLatexMath(latex));
                                                       return map;
                                                   }).collect(Collectors.toList());
         HashMap<Object, Object> param = new HashMap<>();
@@ -99,15 +106,55 @@ public class LatexTest {
         // 把公式中的 $ 和 \  两个符号去掉.
         latex = latex.replaceAll("[$\\\\]", "").trim();
         while (latex.contains("_") || latex.contains("^")) {
-            latex = ReUtil.replaceAll(latex, "(.*?)_\\{(.*?)\\}", "$1<sub>$2</sub>");
-            latex = ReUtil.replaceAll(latex, "(.*?)\\^\\{(.*?)\\}", "$1<sup>$2</sup>");
+            latex = ReUtil.replaceAll(latex, getRegex(latex, "(.*?)_\\{(.*?)\\}"), "$1<sub>$2</sub>");
+            latex = ReUtil.replaceAll(latex, getRegex(latex,"(.*?)\\^\\{(.*?)\\}"), "$1<sup>$2</sup>");
 
             // 部分公式,没有{}大括号      比如:   CO_2
-            latex = ReUtil.replaceAll(latex, "(.*?)_(.)", "$1<sub>$2</sub>");
-            latex = ReUtil.replaceAll(latex, "(.*?)\\^(.)", "$1<sup>$2</sup>");
+            latex = ReUtil.replaceAll(latex, getRegex(latex, "(.*?)_(.)"), "$1<sub>$2</sub>");
+            latex = ReUtil.replaceAll(latex, getRegex(latex, "(.*?)\\^(.)"), "$1<sup>$2</sup>");
         }
         // ABC_{abc_{123}}  ->    ABC<sub>abc<sub>{</sub>123</sub>}
-        latex = ReUtil.replaceAll(latex, "(.*?)<sub>(.*?)<sub>\\{</sub>(.*?)</sub>\\}", "$1<sub>$2<sub>$3</sub></sub>");
+        latex = ReUtil.replaceAll(latex, getRegex(latex, "(.*?)<sub>(.*?)<sub>\\{</sub>(.*?)</sub>\\}"), "$1<sub>$2<sub>$3</sub></sub>");
+        System.out.println(latex);
         return latex;
+    }
+
+    public static String getRegex(String value, String regex) {
+        if (hasNested(value)) {
+            return ReUtil.replaceAll(regex, "\\(\\.\\*\\?\\)", "(.*)");
+        }
+        return regex;
+    }
+
+    /**
+     * 判断 是否存在 {ab{ad}fas} 这种括号嵌套的情况
+     *
+     * <pre>
+     * "fad{adb}"                            false
+     * "asfasf{ad{ddf}dfsdf}"                true
+     * "$$ φ_{C_{2}H_{6}·g} $$"              true
+     * </pre>
+     */
+    public static boolean hasNested(String value) {
+        if (StrUtil.isEmpty(value)) {
+            return false;
+        }
+        int index = -1;
+        byte b = "{".getBytes()[0];
+        byte b2 = "}".getBytes()[0];
+        byte[] bytes = value.getBytes(StandardCharsets.UTF_8);
+        for (int i = 0; i < bytes.length; i++) {
+            if (b == bytes[i]) {
+                if (index != -1) {
+                    return true;
+                }
+                index = i;
+                continue;
+            }
+            if (b2 == bytes[i]) {
+                index = -1;
+            }
+        }
+        return false;
     }
 }
