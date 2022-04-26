e772dc486387cc1edcc1e44cdc30559ad1d99dcd
LiuXianfa <745024471@qq.com>
2022-04-27 00:13:26 +0800

    aaa

--- junit/09-poi/src/main/java/LatexTest.java
+++ junit/09-poi/src/main/java/LatexTest.java
@@ -106,12 +106,14 @@ public class LatexTest {
         // 把公式中的 $ 和 \  两个符号去掉.
         latex = latex.replaceAll("[$\\\\]", "").trim();
         while (latex.contains("_") || latex.contains("^")) {
-            latex = ReUtil.replaceAll(latex, getRegex(latex, "(.*?)_\\{(.*?)\\}"), "$1<sub>$2</sub>");
-            latex = ReUtil.replaceAll(latex, getRegex(latex,"(.*?)\\^\\{(.*?)\\}"), "$1<sup>$2</sup>");
+            latex = ReUtil.replaceAll(latex, hasNested(latex) ? "(.*?)_\\{(.*)\\}" : "(.*?)_\\{(.*?)\\}", "$1<sub>$2</sub>");
+            latex = ReUtil.replaceAll(latex, hasNested(latex) ? "(.*?)\\^\\{(.*)\\}" : "(.*?)\\^\\{(.*?)\\}", "$1<sup>$2</sup>");
 
-            // 部分公式,没有{}大括号      比如:   CO_2
-            latex = ReUtil.replaceAll(latex, getRegex(latex, "(.*?)_(.)"), "$1<sub>$2</sub>");
-            latex = ReUtil.replaceAll(latex, getRegex(latex, "(.*?)\\^(.)"), "$1<sup>$2</sup>");
+            if (!latex.contains("{")) {
+                // 如果公式,没有{}大括号      比如:   CO_2
+                latex = ReUtil.replaceAll(latex, getRegex(latex, "(.*?)_(.)"), "$1<sub>$2</sub>");
+                latex = ReUtil.replaceAll(latex, getRegex(latex, "(.*?)\\^(.)"), "$1<sup>$2</sup>");
+            }
         }
         // ABC_{abc_{123}}  ->    ABC<sub>abc<sub>{</sub>123</sub>}
         latex = ReUtil.replaceAll(latex, getRegex(latex, "(.*?)<sub>(.*?)<sub>\\{</sub>(.*?)</sub>\\}"), "$1<sub>$2<sub>$3</sub></sub>");
