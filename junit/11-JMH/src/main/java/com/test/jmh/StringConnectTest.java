048044a950ce2de9e523f4143c5b81a5b44d65ff
LiuXianfa <745024471@qq.com>
2022-04-28 14:33:19 +0800

    JMH

---
+++ junit/11-JMH/src/main/java/com/test/jmh/StringConnectTest.java
@@ -0,0 +1,64 @@
+package com.test.jmh;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Threads;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.results.format.ResultFormatType;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.concurrent.TimeUnit;
+
+//作者：武培轩
+//        链接：https://www.zhihu.com/question/276455629/answer/1259967560
+//        来源：知乎
+//        著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+@BenchmarkMode(Mode.AverageTime)
+@Warmup(iterations = 3, time = 1)
+@Measurement(iterations = 5, time = 5)
+@Threads(4)
+@Fork(1)
+@State(value = Scope.Benchmark)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+public class StringConnectTest {
+
+    @Param(value = {"100", "500", "1000"})
+    private int length;
+
+    @Benchmark
+    public void testStringAdd(Blackhole blackhole) {
+        String a = "";
+        for (int i = 0; i < length; i++) {
+            a += i;
+        }
+        blackhole.consume(a);
+    }
+
+    @Benchmark
+    public void testStringBuilderAdd(Blackhole blackhole) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            sb.append(i);
+        }
+        blackhole.consume(sb.toString());
+    }
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(StringConnectTest.class.getSimpleName())
+                .result("StringConnectTest_result.json")
+                .resultFormat(ResultFormatType.JSON).build();
+        new Runner(opt).run();
+    }
+}
