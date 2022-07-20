2127698e849e768b043c5f36711888162b89734e
LiuXianfa <745024471@qq.com>
2022-07-20 09:54:28 +0800

    async

--- junit/01-springboot-with-h2/src/test/java/com/liuxianfa/junit/springboot/UserMapperTest.java
+++ junit/01-springboot-with-h2/src/test/java/com/liuxianfa/junit/springboot/UserMapperTest.java
@@ -1,10 +1,13 @@
 package com.liuxianfa.junit.springboot;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import lombok.SneakyThrows;
 
 @SpringBootTest(classes = SpringbootApplication.class)
 @RunWith(value = SpringRunner.class)
@@ -14,13 +17,24 @@ public class UserMapperTest {
     UserMapper userMapper;
 
 
-    @Test
-    public void name() {
-
-        System.out.println(userMapper.save(new User().setId(1L).setName("zzz1").setAge(-1)));
+    @Before
+    public void init() {
+        userMapper.deleteAll();
+        System.out.println(userMapper.save(new User().setId(-1L).setName("zzz1").setAge(-1)));
         System.out.println(userMapper.save(new User().setId(2L).setName("zzz2").setAge(2)));
         System.out.println(userMapper.save(new User().setId(3L).setName("zzz3").setAge(3)));
         System.out.println(userMapper.save(new User().setId(4L).setName("zzz4").setAge(4)));
+    }
 
+
+    @Autowired
+    MainService mainService;
+
+    @SneakyThrows
+    @Test
+    public void name() {
+        mainService.runWithTx();
+
+        Thread.sleep(100 * 1000);
     }
 }
