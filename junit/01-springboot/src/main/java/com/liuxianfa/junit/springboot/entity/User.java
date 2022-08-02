17f73b7e6dbd7d0832d2c58b742aa82bb59d094e
LiuXianfa <745024471@qq.com>
2022-08-02 10:55:27 +0800

    真坑

--- junit/01-springboot/src/main/java/com/liuxianfa/junit/springboot/entity/User.java
+++ junit/01-springboot/src/main/java/com/liuxianfa/junit/springboot/entity/User.java
@@ -14,15 +14,46 @@ public class User {
 
     String name;
 
+    String userName;
+
     /**
      * fixme:这里有个大坑.需要注意!!!
      *  前端传参时,需要传 fageXx 才能接收到参数.
+     *
+     * <pre>
+     *     GET http://localhost:8080/helloList
+     *     Content-Type: application/json
+     *
+     *     [
+     *       {
+     *         "id": 1,
+     *         "name": "xx",
+     *         "userName": "用户名",
+     *         "fageXx": "11",
+     *         "fAddressXx": "河北"
+     *       }
+     *     ]
+     * </pre>
      */
     String fAgeXx;
 
     /**
      * fixme:这里有个大坑.需要注意!!!
      *  前端传参时,需要传 faddressXx 才能接收到参数.
+     * <pre>
+     *     GET http://localhost:8080/helloList
+     *     Content-Type: application/json
+     *
+     *     [
+     *       {
+     *         "id": 1,
+     *         "name": "xx",
+     *         "userName": "用户名",
+     *         "fageXx": "11",
+     *         "fAddressXx": "河北"
+     *       }
+     *     ]
+     * </pre>
      */
     String fAddressXx;
 }
